5f8feb7623b46113fbc62b0b4e2bb7a58cc93035
tothemoon999 <jiang.jingri@yandex.com>
2024-07-11 01:51:31 +0900

    aaaaaaaaaaa

--- src/client.ts
+++ src/client.ts
@@ -27,7 +27,7 @@ async function main() {
 
   // Open connection.
   const client = new Client(args.endpoint, args.xToken, {
-    "grpc.max_receive_message_length": 64 * 1024 * 1024, // 64MiB
+    "grpc.max_receive_message_length": 64 * 1024 * 1024 * 1024, // 64MiB
   });
 
   const commitment = parseCommitmentLevel(args.commitment);
